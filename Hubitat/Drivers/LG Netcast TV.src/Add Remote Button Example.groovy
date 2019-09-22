413b252ee82d77bccce88fbcce990e68ecfa4332
John Goughenour <goug76@gmail.com>
2019-09-22 11:19:05 -0400

    Create Add Remote Button Example.groovy

---
+++ Hubitat/Drivers/LG Netcast TV.src/Add Remote Button Example.groovy
@@ -0,0 +1,37 @@
+public static String version()      {  return "v1.0.0"  }
+public static String name()         {  return "LG Netcast TV"  }
+
+metadata {
+	definition (name: name(), namespace: "goug76", author: "John Goughenour", cstHandler: true) {
+		capability "Audio Volume"
+		capability "Switch"
+		capability "TV"
+        capability "Refresh"
+        capability "Configuration"
+        
+        attribute "session", "string"
+        attribute "maxVolume", "number"
+        attribute "minVolume", "number"
+        attribute "inputSource", "string"
+        attribute "inputLabel", "string"
+        attribute "majorChannel", "string"
+        attribute "minorChannel", "string"
+        attribute "channelName", "string"
+        attribute "currentProgram", "string"
+        attribute "model", "string"
+        
+        command "showKey"
+        command "exampleButton"
+	}
+
+
+	preferences {
+        //CODE
+  }
+}
+
+def exampleButton() {
+    if (detailedLog) log.debug "Executing 'exampleButton'"
+	sendCommand(27)
+    refresh()
+}
