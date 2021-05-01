867248e4da222e62276045d5d2c42503792bd4e0
John Goughenour <goug76@gmail.com>
2021-05-01 10:55:07 -0400

    Create iPhone WiFi Presence Sensor.groovy

---
+++ Hubitat/Drivers/iPhone WiFi Presence Sensor/iPhone WiFi Presence Sensor.groovy
@@ -0,0 +1,110 @@
+/**
+ *  iPhone WiFi Presence Sensor
+ *
+ *  Copyright 2019 Joel Wetzel
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+import groovy.json.*
+	
+metadata {
+	definition (name: "iPhone WiFi Presence Sensor", namespace: "joelwetzel", author: "Joel Wetzel") {
+		capability "Refresh"
+		capability "Sensor"
+        capability "PresenceSensor"
+	}
+
+	preferences {
+		section {
+			input (
+				type: "string",
+				name: "ipAddress",
+				title: "iPhone IP Address",
+				required: true				
+			)
+			input (
+				type: "number",
+				name: "timeoutMinutes",
+				title: "Timeout Minutes",
+				description: "Approximate number of minutes without a response before deciding the device is away/offline.",
+				required: true,
+				defaultValue: 3
+			)
+			input (
+				type: "bool",
+				name: "enableDebugLogging",
+				title: "Enable Debug Logging?",
+				required: true,
+				defaultValue: true
+			)
+		}
+	}
+}
+
+
+def log(msg) {
+	if (enableDebugLogging) {
+		log.debug(msg)	
+	}
+}
+
+
+def installed () {
+	log.info "${device.displayName}.installed()"
+    updated()
+}
+
+
+def updated () {
+	log.info "${device.displayName}.updated()"
+    
+    state.tryCount = 0
+    
+    runEvery1Minute(refresh)		// Generally test it every minute.
+    runIn(2, refresh)				// But test it once, right after we install or update it too.
+}
+
+
+def refresh() {
+	log "${device.displayName}.refresh()"
+
+	state.tryCount = state.tryCount + 1
+    
+    if (state.tryCount > (timeoutMinutes < 1 ? 1 : timeoutMinutes) && device.currentValue('presence') != "not present") {
+        def descriptionText = "${device.displayName} is OFFLINE";
+        log descriptionText
+        sendEvent(name: "presence", value: "not present", linkText: deviceName, descriptionText: descriptionText)
+    }
+    
+	if (ipAddress == null || ipAddress.size() == 0) {
+		return
+	}
+	
+	asynchttpGet("httpGetCallback", [
+		uri: "http://${ipAddress}/"	
+	]);
+}
+
+
+def httpGetCallback(response, data) {
+	//log.debug "${device.displayName}: httpGetCallback(${groovy.json.JsonOutput.toJson(response)}, data)"
+	
+	if (response != null && response.status == 408 && response.errorMessage.contains("Connection refused")) {
+		state.tryCount = 0
+		
+		if (device.currentValue('presence') != "present") {
+			def descriptionText = "${device.displayName} is ONLINE";
+			log descriptionText
+			sendEvent(name: "presence", value: "present", linkText: deviceName, descriptionText: descriptionText)
+		}
+	}
+}
