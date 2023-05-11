ad350c62dec8cb2b61d32fe0a8a9ba52f9fcafc3
This Old Smart Home <goug76@gmail.com>
2023-05-11 15:15:13 -0400

    Update Xfinity ZigBee Contact Sensor.groovy

--- Hubitat/Drivers/Xfinity ZigBee Contact Sensor.src/Xfinity ZigBee Contact Sensor.groovy
+++ Hubitat/Drivers/Xfinity ZigBee Contact Sensor.src/Xfinity ZigBee Contact Sensor.groovy
@@ -21,12 +21,15 @@
 import hubitat.zigbee.clusters.iaszone.ZoneStatus
 import hubitat.device.Protocol
 
-public static String version()      {  return "v1.0.0"  }
-public static String name()         {  return "Xfinity ZigBee Contact Sensor"  }
-def Integer defaultDelay = 333       //default delay to use for zigbee commands (in milliseconds)
+public static String version()          {  return "v1.0.0"  }
+public static String name()             {  return "Xfinity ZigBee Contact Sensor"  }
+public static String driverInfo()       {  return "<p style=\"text-align:center\"></br><strong><a href='https://thisoldsmarthome.com' target='_blank'>This Old Smart Home</a></strong> (tosh)</br>${name()}<br/><em>${version()}</em></p>"  }
+public static Integer defaultDelay()    {  return 333  }    //default delay to use for zigbee commands (in milliseconds)
+public static Double minVolt()          {  return 2.3  }    //default minimum volts to use for battery volt command
+public static Double maxVolt()          {  return 3.0  }    //default maximum volts to use for battery volt command
 
 metadata {
-	definition (name: name(), namespace: "goug76", author: "John Goughenour") {
+	definition (name: name(), namespace: "tosh", author: "John Goughenour") {
         capability "Configuration"
         capability "Sensor"
         capability "ContactSensor"
@@ -39,13 +42,13 @@ metadata {
         
         command "resetBatteryReplacedDate"
         
-        fingerprint deviceJoinName: name(), profileId:"0104", endpointId:"01", inClusters:"0000,0001,0003,0020,0402,0500,0B05,FD50", outClusters:"0019", model:"LDHD2AZW", manufacturer:"Leedarson"
-        fingerprint deviceJoinName: name(), profileId:"0104", endpointId:"01", inClusters:"0000,0001,0003,0020,0402,0500,0B05", outClusters:"0019", model:"URC4460BC0-X-R", manufacturer:"Universal Electronics Inc"
-        fingerprint deviceJoinName: name(), profileId:"0104", endpointId:"01", inClusters:"0000,0001,0003,0020,0402,0500,0B05", outClusters:"0019", model:"MCT-350 SMA", manufacturer:"Visonic"
+        fingerprint profileId:"0104", endpointId:"01", inClusters:"0000,0001,0003,0020,0402,0500,0B05,FD50", outClusters:"0019", model:"LDHD2AZW", manufacturer:"Leedarson"
+        fingerprint profileId:"0104", endpointId:"01", inClusters:"0000,0001,0003,0020,0402,0500,0B05", outClusters:"0019", model:"URC4460BC0-X-R", manufacturer:"Universal Electronics Inc"
+        fingerprint profileId:"0104", endpointId:"01", inClusters:"0000,0001,0003,0020,0402,0500,0B05", outClusters:"0019", model:"MCT-350 SMA", manufacturer:"Visonic"
 	}
     
     preferences {
-        input(name: "infoLogging", type: "bool", title: "<b>Enable Description Test</b>", defaultValue: "true", description: "", required: false)
+        input(name: "infoLogging", type: "bool", title: "<b>Enable Description Text</b>", defaultValue: "true", description: "", required: false)
         input(name: "debugLogging", type: "bool", title: "<b>Enable Debug Logging</b>", defaultValue: "true", description: "", required: false)
         input(name: "batteryInterval", type: "number", title: "<b>Battery Reporting Interval</b>", defaultValue: 12, 
               description: "Set battery reporting interval by this many <b>hours</b>.", required: false)
@@ -54,7 +57,7 @@ metadata {
         input name: "tempOffset", title: "<b>Temperature Calibration</b>", type: "number", range: "-128..127", defaultValue: 0, required: true, 
             description: "Adjust temperature by this many degrees.</br>Range: -128 thru 127</br>Default: 0"
         input name:"about", type: "text", title: "<b>About Driver</b>", 
-            description: "A Zigbee driver for Xfinity door and window sensors. <p style=\"text-align:center\"></br><strong>John Goughenour</strong> (goug76)</br>${name()}<br/><em>${version()}</em></p>"
+            description: "A Zigbee driver for Xfinity door and window sensors. ${driverInfo()}"
   }
 }
 
@@ -74,18 +77,18 @@ def initialize(){
 
 def refresh() {
     def cmd = [
-        "he rattr 0x${device.deviceNetworkId} 1 0x0402 0x0000 {}","delay 200",  //temp
-        "he rattr 0x${device.deviceNetworkId} 1 ${zigbee.POWER_CONFIGURATION_CLUSTER} 0x0020 {}","delay 200",  //battery
+        "he rattr 0x${device.deviceNetworkId} 1 0x0402 0x0000 {}","delay ${defaultDelay}",  //temp
+        "he rattr 0x${device.deviceNetworkId} 1 ${zigbee.POWER_CONFIGURATION_CLUSTER} 0x0020 {}","delay ${defaultDelay()}",  //battery
     ]
     return cmd
 }
 
 def configure() {
     int reportInterval = batteryInterval.toInteger() * 60 * 60
-    List cmd = ["zdo bind 0x${device.deviceNetworkId} 1 1 0x0500 {${device.zigbeeId}} {}", "delay ${defaultDelay}",] // IAS Zone]
-    cmd += zigbee.enrollResponse(1200)
-    cmd += zigbee.configureReporting(zigbee.POWER_CONFIGURATION_CLUSTER, 0x0020, DataType.UINT8, 0, reportInterval, 1, [:], 200)   // Configure Voltage - Report once per 12hrs or if a change of 100mV detected
-    cmd += zigbee.temperatureConfig(0,(tempInterval.toInteger() * 60))
+    List cmd = ["zdo bind 0x${device.deviceNetworkId} 1 1 0x0500 {${device.zigbeeId}} {}", "delay ${defaultDelay()}",] // IAS Zone
+    cmd += zigbee.enrollResponse(1200) // Enroll in IAS Zone
+    cmd += zigbee.configureReporting(zigbee.POWER_CONFIGURATION_CLUSTER, 0x0020, DataType.UINT8, 0, reportInterval, 1, [:], defaultDelay()) //Battery Voltage Reporting
+    cmd += zigbee.temperatureConfig(0,(tempInterval.toInteger() * 60)) // Temperature Reporting
     cmd += refresh()
     
     return cmd
@@ -96,10 +99,10 @@ def resetBatteryReplacedDate() {
 }
 
 void parse(String description) {
-    Map descMap = zigbee.parseDescriptionAsMap(description)    
-    def result = []
+    Map descMap = zigbee.parseDescriptionAsMap(description)
 
     if (description?.startsWith("enroll request")) {
+        def result = []
         List cmds = zigbee.enrollResponse(1200)        
         if(debugLogging) log.debug "enroll response: ${cmds}"        
         result = cmds?.collect { new hubitat.device.HubAction(it, hubitat.device.Protocol.ZIGBEE) }
@@ -119,52 +122,21 @@ private parseIasMessage(String description) {
 }
 
 private parseAttributeMessage(String description) {
-    Map descMap = zigbee.parseDescriptionAsMap(description)
-    switch(descMap.cluster) {
-        case '0402':
-            handleTemperatureEvents(descMap)
-            break
-        case '0001':
-            handleBatteryEvents(descMap)            
-            break
+    Map event = zigbee.getEvent(description)
+    if(event) {
+        switch(event.name) {
+            case 'batteryVoltage':
+                def pct = (event.value - minVolt()) / (maxVolt() - minVolt())
+                def roundedPct = Math.round(pct * 100)
+                if (roundedPct <= 0) roundedPct = 1
+                def descriptionText = "${device.displayName} battery was ${Math.min(100, roundedPct)}%"
+                if(infoLogging) log.info "${descriptionText}"
+                sendEvent(name: 'battery', value: Math.min(100, roundedPct), unit: "%", descriptionText: descriptionText, type: 'physical')
+                break
+            default:
+                if(infoLogging) log.info "${event.descriptionText}"
+                sendEvent(name: event.name, value: event.value, unit: "°${event.unit}", descriptionText: event.descriptionText, type: 'physical')           
+                break
+        }
     }
-}
-
-private handleBatteryEvents(descMap) {
-    def result = [:]
-    if (descMap.value) {
-        def rawValue = zigbee.convertHexToInt(descMap.value)
-        def minVolts = 2.3
-		def maxVolts = 3.0
-		def volts = rawValue / 10
-        def pct = (volts - minVolts) / (maxVolts - minVolts)
-        def roundedPct = Math.round(pct * 100)
-        if (roundedPct <= 0) roundedPct = 1
-        result.value = Math.min(100, roundedPct)
-        result.descriptionText = "${device.displayName} battery is ${result.value}%"
-        result.name = 'battery'
-        result.isStateChange = true
-        result.type = 'physical'
-        result.unit = "%"
-        if(infoLogging) log.info "${result.descriptionText}"
-        sendEvent(result)
-    }
-    return result
-}
-
-private handleTemperatureEvents(descMap) {
-    def result = [:]
-    if (descMap.value) {
-        def rawValue = hexStrToSignedInt(descMap.value) / 100
-        result.value = convertTemperatureIfNeeded(rawValue.toFloat(),"c",2)
-        if(tempOffset) result.value = (result.value.toFloat() + tempOffset.toFloat()).round(2)
-        result.name = 'temperature'
-        result.isStateChange = true
-        result.type = 'physical'
-        result.unit = "°${location.temperatureScale}"
-        result.descriptionText = "${device.displayName} temperature is ${result.value}${result.unit}"
-        if(infoLogging) log.info "${result.descriptionText}"
-        sendEvent(result)
-    }
-    return result
 }
