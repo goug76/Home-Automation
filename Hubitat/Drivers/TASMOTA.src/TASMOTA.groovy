705d1e04a9440f32488e1467279af153265d9832
This Old Smart Home <goug76@gmail.com>
2023-05-20 21:15:19 -0400

    Update TASMOTA.groovy

--- Hubitat/Drivers/TASMOTA.src/TASMOTA.groovy
+++ Hubitat/Drivers/TASMOTA.src/TASMOTA.groovy
@@ -11,9 +11,23 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
- */
+ *
+ ************************************************Change Record**********************************************************
+ *
+ *    Version 1.0.0
+ *        Initial Release
+ *    Version 2.0.0
+ *        Added MQTT support for real time updates.
+ *
+ **********************************************************************************************************************/
+
+public static String version()          {  return "v2.0.0"  }
+public static String name()             {  return "TASMOTA - Basic"  }
+public static String driverInfo()       {  return "<p style=\"text-align:center\"></br><strong><a href='https://thisoldsmarthome.com' target='_blank'>This Old Smart Home</a></strong> (tosh)</br>${name()}<br/><em>${version()}</em></p>"  }
+
 metadata {
-	definition (name: "TASMOTA - Basic", namespace: "goug76", author: "John Goughenour") {
+	definition (name: name(), namespace: "tosh", author: "John Goughenour") {
+        capability "Configuration"
         capability "Actuator"
         capability "Sensor"
         capability "Switch"
@@ -23,12 +37,10 @@ metadata {
         attribute "Uptime" , "string"
         attribute "Version" , "string"
         attribute "Hostname" , "string"
-        attribute "Mac" , "string"
-        attribute "IP" , "string"
-        attribute "Authentication" , "string"
-        attribute "PowerOnState", "string"
-        attribute "AutoOff", "number"
+        attribute "Authentication" , "enum", ["SUCCESS", "FAILED"]
+        attribute "PowerOnState", "enum", ["ON", "OFF", "Previous"]
         attribute "Module", "string"
+        attribute "MqttConnection", "enum", ["CONNECTED", "DISCONNECTED"]
         
         command "reboot"
 	}
@@ -37,40 +49,47 @@ metadata {
     details(["switch", "reboot", "refresh"])
     
     preferences {
-        section ("Configure your Tasmota Device Settings"){
-            input(name: "password", type: "password", description: "Enter Password", title: "Password \nIf you password protected your device", 
-                required: true)
-            input(name: "hostname", type: "bool", title: "Hostname \nChange device Hostname to match device's Friendly Name", 
-                required: false)
-            input(name: "boot", type: "enum", title: "Power on state", description: "Choose a state for when your device first powers on", 
-                options: ["On", "Off", "Previous"], required: false)
-            input(name: "autoOff", type: "number", title: "Auto Off \nAutomatically turn off device after x many seconds \n\nDefault: 0 (Disabled)", 
-                defaultValue: 0, required: false)
-          	input description: "During normal opperation the Tasmota device will only report status changes (On/Off) in the event logger. For more detailed debug logging turn on 'Detailed Logging'", 
-            	type: "paragraph", element: "paragraph", title: "Logging"
-        	input(name: "detailedLog", type: "bool", title: "Detailed Logging", required: false)
-        }
+        input(name: "password", type: "password", title: "<b>Device Password</b>", description: "If you password protected your device</br>Enter Password", required: true)
+        input(name: "mqttPassword", type: "password", title: "<b>MQTT Password</b>", description: "The password for your MQTT Broker.</br>Enter Password", required: true)
+        input(name: "boot", type: "enum", title: "<b>Power on state</b>", options: ["On", "Off", "Previous"], required: false, 
+              description: "Choose a state for when your device first powers on")
+        input(name: "hostname", type: "bool", title: "<b>Hostname</b>", description: "Change device Hostname to match device's Friendly Name", required: false)
+        input(name: "infoLogging", type: "bool", title: "<b>Enable Description Text</b>", defaultValue: "true", description: "", required: false)
+        input(name: "debugLogging", type: "bool", title: "<b>Enable Debug Logging</b>", defaultValue: "true", description: "", required: false)
+        input(name: "autoOff", type: "number", title: "<b>Auto Off</b>", defaultValue: 0, required: false, 
+              description: "Automatically turn off device after x many seconds </br>Default: 0 (Disabled)")
+        input name:"about", type: "text", title: "<b>About Driver</b>", 
+            description: "A LAN driver for devices flashed with the Tasmota firmware. ${driverInfo()}"
      }
 }
 
-// parse events into attributes
-def parse(String description) {
-	log.debug "Parsing '${description}'"
-	// TODO: handle 'switch' attribute
-}
-
 def installed(){
+	if(infoLogging) log.info "${device.displayName} is installing"
 	initialize()
 }
 
 def updated(){
+	if(infoLogging) log.info "${device.displayName} is updating"
 	unschedule()
 	initialize()
 }
 
+def uninstalled(){
+	if(infoLogging) log.info "${device.displayName} is uninstalling"
+    interfaces.mqtt.unsubscribe("${getDataValue("MQTT_Full_Topic")}RESULT")
+    interfaces.mqtt.disconnect()
+}
+
+def configure() {   
+    if(infoLogging) log.info "${device.displayName} is configuring" 
+	unschedule()    
+	initialize()
+}
+
 def initialize(){
-	if (detailedLog) {log.debug "$device.displayName initializing"}
-    if (!device.currentValue("AutoOff") && device.currentValue("AutoOff") != 0) {action("PulseTime", "")}
+    if(infoLogging) log.info "${device.displayName} is  initializing"
+    updateDeviceInfo()
+    pauseExecution(1000)
 	if(boot && boot != device.currentValue("PowerOnState")) {
     	switch(boot) {
             case "On":
@@ -83,16 +102,17 @@ def initialize(){
                 action("PowerOnState", "3")
             	break
         }
-    }   
+    }  
+    
     if(autoOff && autoOff > 0) {
-        def timer = autoOff + 100
-        if(timer != (device.currentValue("AutoOff") + 100)) {
-            action("PulseTime1", timer)
-        }
-    } else if(autoOff != (device.currentValue("AutoOff"))) {    
-    	if (detailedLog) {log.debug "Disabling Auto Off"}
+        def timer = autoOff * 10
+        if (debugLogging) {log.debug "Setting Auto Off to ${timer} seconds"}
+        action("PulseTime1", timer)
+    } else {
+    	if (debugLogging) {log.debug "Disabling Auto Off"}
        	action("PulseTime1", "0")
     }
+    
     if("[" + device.label + "]" != device.currentValue("FriendlyName")) {
         def fName = device.label.replace(" ", "%20")
         action("FriendlyName", fName)
@@ -101,49 +121,60 @@ def initialize(){
         def hName = device.label.replace(" ", "%20")
         action("Hostname", hName)
     }
-    runIn (5, updateDeviceInfo)
-	runEvery1Minute(updateStatus)
+    
+    // Setup MQTT Connection
+    try {
+        interfaces.mqtt.connect("tcp://${getDataValue("MQTT_Broker")}", "hubitat_tasmota", getDataValue("MQTT_User"), mqttPassword)
+        interfaces.mqtt.subscribe("${getDataValue("MQTT_Topic")}RESULT")
+    } catch(Exception e) {
+        log.error "Unable to connect to the MQTT Broker ${e}"
+        sendEvent(name: "MqttConnection", value: 'DISCONNECTED')
+    }
+    
+    if(!interfaces.mqtt.isConnected()) runEvery1Minute(updateStatus)
+	
     runEvery5Minutes(updateDetails)
-    schedule("0 0 2 1/1 * ? *", updateDeviceInfo) // Run Daily
+    //schedule("0 0 2 1/1 * ? *", updateDeviceInfo) // Run Daily
     schedule("0 0 3 ? * SAT *", reboot) // Run Weekly
 }
 
+def refresh() {
+	if(infoLogging) log.info "${device.displayName} is refreshing"
+    updateDetails()
+    updateStatus()
+}
+
 def updateDeviceInfo() {
-	if (detailedLog) {log.debug "$device.displayName device info Update"}
+	if(debugLogging) log.debug "${device.displayName} device info Update"
 	action("Module", "")
     updateDetails()
 }
 
 def updateDetails(){
-	if (detailedLog) {log.debug "$device.displayName detailed update"}
+	if(debugLogging) log.debug "${device.displayName} detailed update"
     action("status", "0")
 }
 
 def updateStatus(){
-	if (detailedLog) {log.debug "$device.displayName refreshing status"}
+	if(debugLogging) log.debug "${device.displayName} refreshing status"
     action("power", "")
 }
 
 // handle commands
-def on() {
-	if (detailedLog) {log.debug "$device.displayName on command received, status: switch - ${device.currentValue("switch")}"}	
+def open() {
+	if(infoLogging) log.info "${device.displayName} on command received, status: switch - ${device.currentValue("switch")}"	
     action ("Power", "on")
     runIn (6, updateStatus)
 }
 
-def off() {
-	if (detailedLog) {log.debug "$device.displayName off command received, status: switch - ${device.currentValue("switch")}"}
-	action("Power", "off")
+def close() {
+	if(infoLogging) log.info "${device.displayName} off command received, status: switch - ${device.currentValue("switch")}"
+	action("Power", "on")
     runIn (6, updateStatus)
 }
 
-def refresh() {
-	if (detailedLog) {log.debug "$device.displayName refresh command received"}
-    updateDeviceInfo()
-}
-
 def reboot() {
-	if (detailedLog) {log.debug "$device.displayName reboot command received, current uptime - ${device.currentValue("Uptime")}"}
+	if(infoLogging) log.info "${device.displayName} reboot command received, current uptime - ${device.currentValue("Uptime")}"
     action("Restart", 1)
     runIn (5, updateDeviceInfo)
 }
@@ -152,16 +183,11 @@ def action(cmd, payload) {
 	def command = cmd
     if(payload) command += "%20${payload}"
 	if (password) command += "&user=admin&password=${password}"
+    if(debugLogging) log.debug "Sending command: ${command}"
     
 	def cmnd = new hubitat.device.HubAction("""GET /cm?cmnd=${command} HTTP/1.1\r\n Accept: */*\r\nHOST: ${device.deviceNetworkId}:80\r\n\r\n"""
     			, hubitat.device.Protocol.LAN, "${device.deviceNetworkId}:80", [callback: callBackHandler])
     sendHubCommand(cmnd)
-}
-
-def parseString(str) {
-	def name = str.split('\\(') [1]
-	name = name.split('\\)') [0]
-	return name	
 }
 
 def getPowerState(num) {
@@ -171,27 +197,52 @@ def getPowerState(num) {
         case 1: powerState = "ON"; break;
         case 3: powerState = "Previous"; break;
     }
+    if(debugLogging) log.debug "${device.displayName} getting power state: ${powerState}"
     return powerState
+}
+
+// parse events into attributes
+def parse(message) {
+    def msg = interfaces.mqtt.parseMessage(message)
+	if(debugLogging) log.debug "Parsing '${msg}'"
+	// TODO: handle 'switch' attribute
+    switch(msg.payload) {
+        case ~/.*POWER.*/:            
+            if(msg.payload.contains("ON")) sendEvent(name: "switch", value: "on") else sendEvent(name: "switch", value: "off")
+            break
+        case ~/.*Action.*/:
+            log.error "Parser Action"
+            break
+    }
+}
+
+def mqttClientStatus(message) {
+    if(debugLogging) log.debug "MQTT Client Status: ${message}"
+    switch(message) {
+        case ~/.*Connection succeeded.*/:
+            sendEvent(name: "MqttConnection", value: 'CONNECTED')
+            break
+    }
 }
 
 void callBackHandler(hubitat.device.HubResponse hubResponse) {
 	state.status = hubResponse.body
-    if (detailedLog) {log.debug "$device.displayName responded with '$state.status'"}
+    if(debugLogging) log.debug "${device.displayName} responded with '${state.status}'"
     if (state.status.contains('WARNING')) {
-    	log.error "$device.displayName (${device.deviceNetworkId}) password failure"
+        log.error "${device.displayName} (${device.deviceNetworkId}) password failure"
         sendEvent(name: "Authentication", value: 'FAILED')
     }
     else {
     	def reportedState
     	def msg = new groovy.json.JsonSlurper().parseText(hubResponse.body)
+        if(debugLogging) log.debug "${device.displayName} responded with '${msg}'"
     	sendEvent(name: "Authentication", value: 'SUCCESS')        
         switch(msg) {
         	case ~/\{POWER=.*/:
             	reportedState = msg.POWER.toLowerCase()
           		break
          	case ~/\{Module=.*/:
-            	def mod = parseString(msg.Module)
-                sendEvent(name: "Module", value: mod)
+                sendEvent(name: "Module", value: msg.Module)
             	break
           	case ~/\{Status=.*/:
             	def SonoffPower = msg.Status.Power
@@ -202,37 +253,41 @@ void callBackHandler(hubitat.device.HubResponse hubResponse) {
                 sendEvent(name: "Uptime", value: "$SonoffUptime")
                 sendEvent(name: "FriendlyName", value: msg.Status.FriendlyName)
                 sendEvent(name: "Version", value: msg.StatusFWR.Version)
-                sendEvent(name: "Hostname", value: msg.StatusNET.Hostname)
-                sendEvent(name: "Mac", value: msg.StatusNET.Mac)
-                sendEvent(name: "IP", value: msg.StatusNET.IPAddress)
+                sendEvent(name: "Hostname", value: msg.StatusNET.Hostname)   
                 if (SonoffPower == 1) {reportedState = "on"} else {reportedState = "off"}
+                updateDataValue("IP", msg.StatusNET.IPAddress)  
+                updateDataValue("Mac", msg.StatusNET.Mac) 
+                if(msg.containsKey("StatusMQT")) {
+                    updateDataValue("MQTT_Broker", "${msg.StatusMQT.MqttHost}:${msg.StatusMQT.MqttPort}")
+                    updateDataValue("MQTT_User", msg.StatusMQT.MqttUser)  
+                    updateDataValue("MQTT_Topic", "stat/${msg.Status.Topic}/")
+                } else {
+                    removeDataValue("MQTT_Broker")
+                    removeDataValue("MQTT_User")  
+                    removeDataValue("MQTT_Topic") 
+                    removeDataValue("MQTT_Full_Topic")
+                }
             	break 
         	case ~/\{PowerOnState=.*/:
             	def currentBoot = device.currentValue("PowerOnState")
                 def powerState = getPowerState(msg.PowerOnState)
-            	if (detailedLog) {log.debug "$device.displayName updated: PowerOnState from: $currentBoot to $powerState"}
+                if(debugLogging) log.debug "${device.displayName} updated: PowerOnState from: ${currentBoot} to ${powerState}"
             	sendEvent(name: "PowerOnState", value: powerState)
                 break
-        	case ~/\{PulseTime1=.*/:
-            	def bTime = (msg.PulseTime1.Set).toInteger() - 100
-                def currentOff = device.currentValue("AutoOff")
-            	if (detailedLog && currentOff != bTime) {log.debug "$device.displayName updated: AutoOff from: $currentOff seconds to $bTime seconds"}
-                sendEvent(name: "AutoOff", value: bTime, unit: "seconds")
-            	break
           	case ~/\{Hostname=.*/:
             	def currentHostname = device.currentValue("Hostname")
-            	if (detailedLog) {log.debug "$device.displayName updated: Hostname from: $currentHostname to ${msg.Hostname}"}
+            if(debugLogging) log.debug "${device.displayName} updated: Hostname from: ${currentHostname} to ${msg.Hostname}"
             	sendEvent(name: "Hostname", value: msg.Hostname)
                 break
           	case ~/\{FriendlyName1=.*/:
             	def currentFriendly = device.currentValue("FriendlyName")
-            	if (detailedLog) {log.debug "$device.displayName updated: FriendlyName from: $currentFriendly to ${msg.FriendlyName1}"}
+            if(debugLogging) log.debug "${device.displayName} updated: FriendlyName from: ${currentFriendly} to ${msg.FriendlyName1}"
             	sendEvent(name: "FriendlyName", value: msg.FriendlyName1)
                 break   
         }
         def currentState = device.currentValue("switch")
         sendEvent(name: "refresh", value: "active")
-        if (reportedState){sendEvent(name: "switch", value: reportedState)}
+        if (reportedState) sendEvent(name: "switch", value: reportedState)
         def SwitchState = device.currentValue("switch")
         if ((SwitchState != currentState) && (SwitchState)) {log.debug "$device.displayName at $device.deviceNetworkId turned $SwitchState"}
   	}    
