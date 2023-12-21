724384a226acb551219f8bd30e7ec4259accb819
John Goughenour <7610738+goug76@users.noreply.github.com>
2023-12-21 17:15:52 -0500

    Update Advanced Presence Sensor.groovy

--- Hubitat/Drivers/Advanced Presence Sensor.scr/Advanced Presence Sensor.groovy
+++ Hubitat/Drivers/Advanced Presence Sensor.scr/Advanced Presence Sensor.groovy
@@ -12,12 +12,12 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- ************************************************Change Record**********************************************************
+ *******************************Change Record*****************************************
  *
  *    Version 1.0.0
  *        Initial Release
  *
- **********************************************************************************************************************/
+ ****************************************************************************************/
 
 public static String version()          {  return "v1.0.0"  }
 public static String name()             {  return "Advanced Presence Sensor"  }
