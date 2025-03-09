f86270afa8ca0a25893f27580e7299459bf640bc
John Goughenour <goug76@gmail.com>
2025-03-09 12:24:00 -0400

    Updated Smart TV Connect App

--- Hubitat/Apps/Smart TV (Connect).src/Smart TV (Connect).groovy
+++ Hubitat/Apps/Smart TV (Connect).src/Smart TV (Connect).groovy
@@ -1,30 +1,47 @@
 /**
  *  Smart TV (Connect)
  *
- *  Copyright 2019 John Goughenour
+ *  MIT License
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
+ *  Copyright (c) 2023 This Old Smart Home
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
  *
- ************************************************Change Record**********************************************************
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
  *
- *    Version 1.0.0
- *                    Initial Release
- *
- **********************************************************************************************************************/
+ */
 
-public static String version()      {  return "v1.0.0"  }
+public static String version()      {  return "v2.0.0"  }
+public static String name()         {  return "Smart TV (Connect)"  }
+public static String codeUrl()
+{
+    return "https://raw.githubusercontent.com/goug76/Home-Automation/refs/heads/master/Hubitat/Apps/Smart%20TV%20(Connect).src/Smart%20TV%20(Connect).groovy"
+}
+public static String driverInfo()
+{
+    return """
+        <p style='text-align:center'>
+        <strong><a href='https://thisoldsmarthome.com' target='_blank'>This Old Smart Home</a></strong> (TOSH-SmartHome)</br>
+        ${name()}</br>
+        <em>${version()}</em></p>
+    """
+}
 
-definition(
-    name: "Smart TV (Connect)",
-    namespace: "goug76",
-    author: "John Goughenour",
+definition (name: name(), namespace: "tosh", author: "John Goughenour", importUrl: codeUrl(),
     description: "Smart app to discover and subscribe to TV events for Smart TVs.",
     category: "My Apps",
     parent: "goug76:Smart Home Automation",
@@ -73,7 +90,7 @@ def mainPage() {
             mode title: "Only run during specific mode(s):", required: false
         }
         section("<p style=\"border-bottom:1px solid\">About</p>") {
-            paragraph "<p style=\"text-align:center\"><strong>John Goughenour</strong></br>${app.name}</br><em>${version()}</em></p>"
+            paragraph "${driverInfo()}"
         }
     }    
 }
--- Hubitat/Drivers/LG Netcast TV.src/LG Netcast TV.groovy
+++ Hubitat/Drivers/LG Netcast TV.src/LG Netcast TV.groovy
@@ -1,29 +1,48 @@
 /**
  *  LG Netcast TV
  *
- *  Copyright 2019 John Goughenour
+ *  MIT License
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
+ *  Copyright (c) 2023 This Old Smart Home
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
  *
- ************************************************Change Record**********************************************************
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
  *
- *    Version 1.0.0
- *                    Initial Release
- *
- **********************************************************************************************************************/
+ */
 
 public static String version()      {  return "v1.0.0"  }
 public static String name()         {  return "LG Netcast TV"  }
+public static String codeUrl()
+{
+    return "https://raw.githubusercontent.com/goug76/Home-Automation/refs/heads/master/Hubitat/Drivers/LG%20Netcast%20TV.src/LG%20Netcast%20TV.groovy"
+}
+public static String driverInfo()
+{
+    return """
+        <p style='text-align:center'></br>
+        <strong><a href='https://thisoldsmarthome.com' target='_blank'>This Old Smart Home</a></strong> (TOSH-SmartHome)</br>
+        ${name()}</br>
+        <em>${version()}</em></p>
+    """
+}
 
 metadata {
-	definition (name: name(), namespace: "goug76", author: "John Goughenour", cstHandler: true) {
+	definition (name: name(), namespace: "tosh", author: "John Goughenour", importUrl: codeUrl(), cstHandler: true) {
 		capability "Audio Volume"
 		capability "Switch"
 		capability "TV"
@@ -59,7 +78,8 @@ metadata {
         input (name: "pairingKey", type: "text", title: "<b>Pairing Key</b>", defaultValue: "", required: true)
         input ("heartbeat", "enum", title: "<b>Heartbeat</b>", options: poll, defaultValue: 60, description: "Set polling intervals from every 5 seconds to every 10 minutes<br><i>Default: </i><font color=\"red\">Every 1 minute</font>")
         input(name: "detailedLog", type: "bool", title: "<b>Detailed Logging</b>", defaultValue: "false", description: "During normal opperation the ${name()} will only report status changes (On/Off) in the event log. For more detailed debug logging turn on 'Detailed Logging'", required: false)
-        input name:"about", type: "text", title: "<b>About Driver</b>", description: "<p style=\"text-align:center\"><strong>John Goughenour</strong> (goug76)</br>${name()}<br/><em>${version()}</em></p>"
+        input name:"about", type: "text", title: "<b>About Driver</b>", 
+            description: "LG Netcast driver used with the Smart TV (Connect) app. ${driverInfo()}"
     }
 }
 
